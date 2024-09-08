456b4578af5460dd851828342f1fc31482920c06
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-09-08 15:58:53 +0530

    [Rahul] | Add. Sidebar Component

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import Image from "next/image";
+import {CalendarMonth, LocationOn, MailOutline, PhoneAndroid, SvgIconComponent} from "@mui/icons-material";
+import styles from "./sidebar.module.css";
+
+interface SidebarProps {
+    icon: SvgIconComponent;
+    title: string;
+    content: JSX.Element;
+}
+
+const Sidebar: React.FC = () => {
+    return (
+        <aside className={styles.sidebar}>
+            <div className={styles.sidebarInfo}>
+                <figure className={styles.avatarBox}>
+                    <Image src="/images/profile.png" priority={true} alt="Rahul Ramesh" width={150} height={150} />
+                </figure>
+                <div className={styles.infoContent}>
+                    <h1 className={styles.name} title="Rahul Ramesh">
+                        Rahul Ramesh
+                    </h1>
+                    <p className={styles.title}>Developer</p>
+                </div>
+                <div className={styles.sidebarInfoMore}>
+                    <div className={styles.separator}></div>
+                    <ul className={styles.sidebarItemList}>
+                        <SidebarItem
+                            icon={MailOutline}
+                            title="Email"
+                            content={
+                                <a href="mailto:rahul.ramesh.workmail@gmail.com">rahul.ramesh.workmail@gmail.com</a>
+                            }
+                        />
+                        <SidebarItem
+                            icon={PhoneAndroid}
+                            title="Phone"
+                            content={<a href="tel:+918086237540">+91 8086-2375-40</a>}
+                        />
+                        <SidebarItem
+                            icon={CalendarMonth}
+                            title="Birthday"
+                            content={<time dateTime="2000-06-14">June 14, 2000</time>}
+                        />
+                        <SidebarItem
+                            icon={LocationOn}
+                            title="Location"
+                            content={<address>Kodungallur, Thrissur, Kerala</address>}
+                        />
+                    </ul>
+                    <div className={styles.separator}></div>
+                </div>
+            </div>
+        </aside>
+    );
+};
+
+const SidebarItem: React.FC<SidebarProps> = ({icon: Icon, title, content}) => (
+    <li className={styles.sidebarItem}>
+        <div className={styles.iconBox}>
+            <Icon fontSize="small" />
+        </div>
+        <div className={styles.sidebarItemInfo}>
+            <p className={styles.sidebarItemTitle}>{title}</p>
+            {content}
+        </div>
+    </li>
+);
+
+export default Sidebar;
