27a33c1cba121d3ab78149c4c8b3244e98ee13f7
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-11-03 21:21:24 +0530

    [Rahul] | Update. Github Pages Deployement Setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  assetPrefix: "./",
-  webpack: (config) => {
-    config.experiments = config.experiments || {};
-    config.experiments.topLevelAwait = true;
-    return config;
-  },
-  images: {
-    unoptimized: true,
-  },
+    basePath: "/portfolio-website",
+    output: "export",
+    assetPrefix: "./",
+    webpack: (config) => {
+        config.experiments = config.experiments || {};
+        config.experiments.topLevelAwait = true;
+        return config;
+    },
+    images: {
+        unoptimized: true,
+    },
 };
 export default nextConfig;
