71c7f8b4ec16c39a5f38763ee16a50879db050ca
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-11-03 21:12:37 +0530

    [Rahul] | Add. Main Content Component

---
+++ src/components/mainContent.tsx
@@ -0,0 +1,19 @@
+"use client";
+import React, {useState} from "react";
+import styles from "./mainContent.module.css";
+import Navbar from "./navbar";
+import Content from "./content";
+import {ComponentName, COMPONENTS} from "./constants";
+
+const MainContent: React.FC = () => {
+    const [displayedTable, setDisplayedTable] = useState<ComponentName>("About");
+
+    return (
+        <div className={styles.mainContent}>
+            <Navbar setDisplayedTable={setDisplayedTable} activeTab={displayedTable} />
+            <Content displayedComponent={COMPONENTS[displayedTable]} />
+        </div>
+    );
+};
+
+export default MainContent;
