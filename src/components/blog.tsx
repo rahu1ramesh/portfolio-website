a3f9d4496fae51e84c96f146e4fafc5366663433
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-11-04 22:04:06 +0530

    [Rahul] | Add. Blog Page

--- src/components/blog.tsx
+++ src/components/blog.tsx
@@ -2,7 +2,12 @@ import React from "react";
 import styles from "../styles/blog.module.css";
 
 const Blog: React.FC = () => {
-    return <div className={styles.blog}></div>;
+    return (
+        <div className={styles.blog}>
+            <h2 className={styles.blogTitle}>My Blog</h2>
+            <p className={styles.blogContent}>Coming Soon...</p>
+        </div>
+    );
 };
 
 export default Blog;
