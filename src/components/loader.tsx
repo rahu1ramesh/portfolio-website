bbf9977d9fb286be7de7893e423f7b24d398242d
Rahul Ramesh <rahul.ramesh@thoughtworks.com>
2024-11-06 15:44:00 +0530

    [Rahul] | Add. Loader Component

---
+++ src/components/loader.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "../styles/loader.module.css";
+
+const Loader: React.FC = () => {
+    return <div className={styles.loader}></div>;
+};
+
+export default Loader;
